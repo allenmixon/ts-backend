bbaefaeab69265a5df8ec7b0bf8262f9891a2a97
Allen Mixon <johnamixon@gmail.com>
2021-12-14 21:09:43 -0800

    flask app

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
-#comment
-#comment2
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/call-ready', methods=['POST'])
+def call_ready():
+	return jsonify(success=True)
+
+if __name__ == "__main__":
+	app.run(debug=True)
