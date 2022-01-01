1d781002ae1f483d2902cca9b97775fd6d287e64
Allen Mixon <johnamixon@gmail.com>
2022-01-01 15:58:27 -0800

    enable cors

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-#test12
+import psycopg2
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 #Call Ready Endpoint
 @app.route('/call-ready', methods=['POST'])
