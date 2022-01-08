0ceed40410e48ca2077ecb2fe8708486595af30a
Allen Mixon <johnamixon@gmail.com>
2022-01-07 19:02:01 -0800

    few changes

--- run.py
+++ run.py
@@ -17,12 +17,16 @@ def token_required(f):
 		token = request.args.get('token')
 
 		if not token:
-			return jsonify({'message' : 'Token is missing'}), 403
+			return jsonify({
+				'message' : 'Token is missing'
+			}), 403
 
 		try:
 			data = jwt.decode(token, app.config['SECRET_KEY'])
 		except:
-			return jsonify({'message' : 'Token is invalid'}), 403
+			return jsonify({
+				'message' : 'Token is invalid'
+			}), 403
 
 		return f(*args, **kwargs)
 
@@ -37,23 +41,23 @@ def login():
 	if password == 'starchild':
 		token = jwt.encode({'user' : user, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=1)}, app.config['SECRET_KEY'])
 		#response = jsonify({'token' : token.decode('UTF-8')})
-		response = jsonify({'token' : token})
 
-		return response
+		return jsonify({
+			'login' : 'valid'
+			'token' : token
+		})
 
-	return 'Invalid login'
-
-#For testing
-@app.route('/protected')
-@token_required
-def protected():
-	return '/protected - Valid login'
+	return jsonify({
+		'login' : 'invalid'
+	})
 
 #Call Ready Endpoint
 @app.route('/call-ready', methods=['POST'])
 @token_required
 def call_ready():
-	return "Awesome yessirrr"
+	return jsonify({
+			'status' : 'submitted'
+		})
 
 if __name__ == '__main__':
     app.run(host='treeserver', port=5001)
