eea0de9c7180ed451a0991a28a1171fcb362f54c
Allen Mixon <johnamixon@gmail.com>
2022-05-14 20:17:27 -0700

    nginx containerized

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-python local.py
+if [ "$APP_ENV" == "dev" ]; then
+	python local.py
+else
+	mkdir -p /var/log/gunicorn
+	touch /var/log/gunicorn/error.log
+	touch /var/log/gunicorn/access.log
+	tail -fn 20 /var/log/gunicorn/*.log &
+
+	gunicorn -c config/gunicorn.py "app:create_app()"
+fi
